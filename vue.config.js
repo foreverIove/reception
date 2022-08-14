2b372cd351ee292bff0fbb12b0bb92086bbde440
foreverIove <105284937+foreverIove@users.noreply.github.com>
2022-08-14 21:53:09 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  productionSourceMap:false ,
+  transpileDependencies: true,
+   // 关闭语法检查
+   lintOnSave: false,
+   //.代理跨域
+   devServer: {
+    
+    proxy: {
+      '/api': {
+        target: 'http://gmall-h5-api.atguigu.cn',
+        ws: true,
+        changeOrigin: true
+      },
+    //  -------------------
+      
+
+    // -----------------
+      
+    },
+ 
+  }
+})
+
+
